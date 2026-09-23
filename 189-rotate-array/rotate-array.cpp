c7eb4156e5bbef63300ab0860e005f3de8d367f3
Agrima _ Bajpai_7 <agrimabajpai933641@gmail.com>
2026-09-24 02:44:43 +0530

    Time: 4 ms (38.40%) | Memory: 263.5 MB (5.53%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k % n;
-
-        vector<int> temp(k);
-
-        // Store last k elements
-        for(int i = 0; i < k; i++) {
-            temp[i] = nums[n - k + i];
-        }
-
-        // Shift remaining elements to the right
-        for(int i = n - k - 1; i >= 0; i--) {
-            nums[i + k] = nums[i];
-        }
-
-        // Put last k elements at the beginning
-        for(int i = 0; i < k; i++) {
-            nums[i] = temp[i];
-        }
+        int n=nums.size();
+        k=k%n;
+         reverse(nums.begin(),nums.end());
+      reverse(nums.begin(),nums.begin()+k);
+      reverse(nums.begin()+k,nums.end());
+     
     }
 };
